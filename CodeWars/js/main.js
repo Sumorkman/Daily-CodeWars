f97bcc56b79deb016a3f40aeb9a642f94f73e00a
Sumorkman <101617971+Sumorkman@users.noreply.github.com>
2022-06-04 21:37:25 -0700

    Add files via upload

--- CodeWars/js/main.js
+++ CodeWars/js/main.js
@@ -5,12 +5,21 @@
 //   }
 
 // Remove First and Last Character
-const removeChar = str => {
-  let newStr = ''
-  for(let i = 0; i < str.length; i++){
-      if(i !== 0 && i !== str.length -1) {
-          newStr += str[i]
-      }
+// const removeChar = str => {
+//   let newStr = ''
+//   for(let i = 0; i < str.length; i++){
+//       if(i !== 0 && i !== str.length -1) {
+//           newStr += str[i]
+//       }
+//   }
+//   return newStr
+// 
+
+// Convert boolean values to strings 'Yes' or 'No'
+function boolToWord( bool ){
+  if (bool) {
+    return 'Yes';
+  } else {
+    return 'No';
   }
-  return newStr
 }
